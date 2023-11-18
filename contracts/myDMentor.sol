9d11235e78a2d585bc53365c90208097afb52d42
maozkurt <maozkurt12@gmail.com>
2023-11-18 21:15:41 +0300

    some mappings & functions

--- contracts/myDMentor.sol
+++ contracts/myDMentor.sol
@@ -4,7 +4,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract myDMentor is Ownable{
-    constructor(address initialOwner) Ownable(initialOwner) {
 
+    mapping(address => bool) private ifVerified;
+
+    mapping(address => mapping(address => int256)) private requestId;
+    mapping(int256 => string) private requestHash;
+    mapping(int256 => bool) requestVerified;
+
+    constructor(address initialOwner) Ownable(initialOwner) {
+        ifVerified[initialOwner] = true;
     }
+
+    function getIfVerified(address _address) public view returns(bool){
+        return ifVerified[_address];
+    }
+
+    // 
+    function setVerified(address _address, bool _verified) public onlyOwner{
+        ifVerified[_address] = _verified;
+    }
+
+    function getRequestId(address publisher, address receiver) public view returns(int256){
+        require(msg.sender == publisher || msg.sender == receiver || msg.sender == this.owner());
+        return requestId[publisher][receiver];
+    }
+
+
+
+
 }
